8876ddeeeb52b22d7dded44c722ecb317928bd53
Moham-D22 <m0791589468@gmail.com>
2025-05-24 02:00:20 +0300

    fix port issue

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import os
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def home():
     return {"message": "Hello from Railway!"}
+
+if _name_ == "_main_":
+    port = int(os.environ["PORT"])
+    uvicorn.run(app, host="0.0.0.0", port=port)
